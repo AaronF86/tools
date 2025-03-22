bccf052f8b3bc83876db6bf8b4c6663bc04a271e
aaron <aaronfulton86@gmail.com>
2025-03-22 18:15:14 +0000

    [bannify] fix: nix build

--- bannify/nix/build.nix
+++ bannify/nix/build.nix
@@ -4,16 +4,28 @@ pkgs.rustPlatform.buildRustPackage {
   pname = "bannify";
   version = "0.1.0";
 
-  src = ./.;
+  src = ../.;
 
-  cargoLock.lockFile = ./Cargo.lock;
+  cargoLock.lockFile = ../Cargo.lock;
 
-  nativeBuildInputs = with pkgs; [ ];
+  nativeBuildInputs = with pkgs; [
+    pkgs.rustPlatform.cargoSetupHook
+  ];
 
   buildInputs = with pkgs; [
     openssl
     zlib
   ];
+
+  buildPhase = ''
+    export CARGO_HOME=$(mktemp -d)
+    cargo build --release
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp target/release/bannify $out/bin/bannify
+  '';
 
   postInstall = ''
     mkdir -p $out/share/bannify
@@ -26,7 +38,7 @@ pkgs.rustPlatform.buildRustPackage {
 
   meta = with pkgs.lib; {
     description = "being a Comment Banner Tool";
-    homepage = "https://github.com/AaronF86/bannify";
+    homepage = "https://github.com/AaronF86/tools/bannify";
     license = licenses.mit;
     maintainers = with maintainers; [ AaronF86 ];
     platforms = platforms.all;
